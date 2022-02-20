57bb12637e7b9cdf6cd4d8a3bf701589ae0cd24c
Poorv-34 <80013101+Poorv-34@users.noreply.github.com>
2022-02-20 17:50:49 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ difference = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(500, 500);
-    canvas = createCanvas(600, 600);
-    canvas.position(560, 160);
+    canvas = createCanvas(550, 550);
+    canvas.position(550, 150);
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
@@ -15,8 +15,16 @@ function setup(){
 function gotPoses(results){
     if (results.length > 0) {
         console.log(results);
-        leftWristX = results[0].pose.leftWristX.x;
-        rightWristX = results[0].pose.rightWristX.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
         difference = floor(leftWristX - rightWristX);
     }
+}
+
+function draw(){
+    background('#32cc7c');
+    textSize(difference);
+    fill('#00FF00');
+    text('Hello', 50, 30);
+    document.getElementById("font_size").innerHTML = "Width and Height of the Square will be" + difference + "px";
 }
